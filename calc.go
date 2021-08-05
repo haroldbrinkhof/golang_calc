a5e8ba89c3e249c380a6655c62d57186c6ab5899
Harold Brinkhof <harold.brinkhof@gmail.com>
2021-08-06 00:34:02 +0200

    added cli interface

--- calc.go
+++ calc.go
@@ -1,7 +1,54 @@
 package main
-import ( "fmt" )
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"catsandcoding.be/calc/parser"
+)
 
 func main() {
-	fmt.Println("calc")
+	useGUI := false
+	showCalculations := false
+
+	// cli flag parsing
+	flag.BoolVar(&useGUI, "g", false, "starts the gui")
+	flag.BoolVar(&showCalculations, "s", false, "show initial term before result, e.g. 1 + 2 = 3")
+	flag.Parse()
+	// handle any extra terms given
+	calculateAndPrint(flag.Args(), showCalculations)
+
+	// handle terms passed via pipe
+	bytes, err := ioutil.ReadAll(os.Stdin)
+	if err == nil {
+
+		lines := strings.Split(string(bytes), "\n")
+		calculateAndPrint(lines, showCalculations)
+
+	} else {
+		log.Println(err)
+	}
+}
+
+func calculateAndPrint(lines []string, showCalculations bool) {
+	for i := 0; i < len(lines); i++ {
+		if len(strings.TrimSpace(lines[i])) > 0 {
+			total, err := parser.Calculate(lines[i])
+			var outcome string
+			if err == nil {
+				outcome = fmt.Sprintf("%f", total)
+			} else {
+				outcome = err.Error()
+			}
+			if showCalculations {
+				fmt.Printf("%s = %s\n", lines[i], outcome)
+			} else {
+				fmt.Println(outcome)
+			}
+		}
+	}
 }
